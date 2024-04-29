0e18942e03909c6c879468b9ff72b3dc86354ce2
Om Mehta <ommehta16@icloud.com>
2024-04-29 13:42:57 -0400

    Added collide and bounce methods for ball

--- Ball.java
+++ Ball.java
@@ -2,26 +2,53 @@ public class Ball {
     private double[] position;
     private double radius;
     private double elasticness;
+    private double gravity;
     private double[] velocity;
+    final private double tiny = 0.000000001;
     
     public Ball(double[] pos, double[] vel, double r, double elastic) {
         position = pos;
         velocity = vel;
         radius = r;
         elasticness = elastic;
+        gravity = 9.8 * 0.001; //9.5m/s^2
     }
 
     public Ball() {
         position = new double[] {0,0};
+        velocity = new double[] {0,0};
         radius = 0.1;
-        elasticness = 1;
+        elasticness = 0.5;
+        gravity = 0.001;
+    }
+    public void move() {
+        position[0] += velocity[0];
+        position[1] += velocity[1];
+        velocity[1] -= gravity;
+    }
+    
+    public void bounceWall() {
+        if (position[0] - radius <= -1 || position[0] + radius >= 1) velocity[0] *= -elasticness;
+        if (position[1] - radius <= -1 || position[1] + radius >= 1) velocity[1] *= -elasticness;
+
     }
 
     public void collide(Ball otherBall) {
-        // collides with a Ball
-        return;
-    }
+        double dist = Math.sqrt(Math.pow(otherBall.position[0] - position[0],2) + Math.pow(otherBall.position[1] - position[1],2));
+        if (dist > radius + otherBall.radius) return;
+        
+        double velMag = Math.sqrt(Math.pow(velocity[0],2) + Math.pow(velocity[1],2));
+        double othMag = Math.sqrt(Math.pow(otherBall.velocity[0],2) + Math.pow(otherBall.velocity[1],2));
+        
+        double diffX = position[0]-otherBall.position[0];
+        double diffY = position[1]-otherBall.position[1];
 
+        velocity = new double[] {velMag * diffX/dist, velMag * diffY/dist};
+        otherBall.velocity = new double[] {-othMag * diffX/dist, -othMag * diffY/dist};
+
+
+    }
+    
     public void collide(Square otherSquare) {
         // collides with a Square
         return;
--- Sandbox.java
+++ Sandbox.java
@@ -1,6 +1,24 @@
 public class Sandbox {
     public static void main(String[] args) {
         System.out.println("exampletext");
+        Ball ball1 = new Ball();
+        Ball ball2 = new Ball(new double[] {0,0.5}, new double[] {0,0}, 0.1, 0.9);
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-1.0, 1.0);
         StdDraw.enableDoubleBuffering();
+        
+
+        while (true) {
+            StdDraw.clear(StdDraw.WHITE);
+            ball1.move();
+            ball2.move();
+            ball1.bounceWall();
+            ball2.bounceWall();
+            ball1.collide(ball2);
+            StdDraw.filledCircle(ball1.getPos()[0], ball1.getPos()[1], ball1.getRad());
+            StdDraw.filledCircle(ball2.getPos()[0], ball2.getPos()[1], ball2.getRad());
+            StdDraw.show();
+            StdDraw.pause(20);
+        }
     }
 }
