9da18dea5be7b81f65e4b8a16535f5989e32d2b7
Om Mehta <ommehta16@icloud.com>
2024-05-02 13:25:39 -0400

    Added colors, collisions, squares

--- Ball.java
+++ Ball.java
@@ -1,17 +1,22 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
 public class Ball {
     private double[] position;
     private double radius;
     private double elasticness;
     private double gravity;
     private double[] velocity;
-    final private double tiny = 0.000000001;
+    private Color color;
+    final Color[] colors = {StdDraw.RED, StdDraw.ORANGE, StdDraw.YELLOW, StdDraw.GREEN, StdDraw.BLUE, StdDraw.MAGENTA, StdDraw.BLACK, StdDraw.CYAN, StdDraw.LIGHT_GRAY, StdDraw.PINK, StdDraw.PINK, StdDraw.YELLOW, StdDraw.DARK_GRAY};
     
-    public Ball(double[] pos, double[] vel, double r, double elastic) {
+    public Ball(double[] pos, double[] vel, double r, double elastic, Color col) {
         position = pos;
         velocity = vel;
         radius = r;
         elasticness = elastic;
-        gravity = 9.8 * 0.001; //9.5m/s^2
+        gravity = 0.001;
+        color = col;
     }
 
     public Ball() {
@@ -20,6 +25,7 @@ public class Ball {
         radius = 0.1;
         elasticness = 0.5;
         gravity = 0.001;
+        color = colors[(int)(Math.random() * colors.length)];
     }
     public void move() {
         position[0] += velocity[0];
@@ -28,32 +34,75 @@ public class Ball {
     }
     
     public void bounceWall() {
-        if (position[0] - radius <= -1 || position[0] + radius >= 1) velocity[0] *= -elasticness;
-        if (position[1] - radius <= -1 || position[1] + radius >= 1) velocity[1] *= -elasticness;
+        if (position[0] - radius <= -1) {velocity[0] *= -elasticness; position[0] = (-1+radius + 3*position[0])/4;}
+        if (position[0] + radius >= 1)  {velocity[0] *= -elasticness; position[0] = (1-radius + 3*position[0])/4;}
+        if (position[1] - radius <= -1) {velocity[1] *= -elasticness; position[1] = (-1+radius+3*position[1])/4;}
+        if (position[1] + radius >= 1)  {velocity[1] *= -elasticness; position[1] = (1-radius+3*position[1])/4;}
 
     }
 
-    public void collide(Ball otherBall) {
+    public boolean collide(Ball otherBall) {
         double dist = Math.sqrt(Math.pow(otherBall.position[0] - position[0],2) + Math.pow(otherBall.position[1] - position[1],2));
-        if (dist > radius + otherBall.radius) return;
+        
+        if (dist > radius + otherBall.radius) return false;
         
         double velMag = Math.sqrt(Math.pow(velocity[0],2) + Math.pow(velocity[1],2));
         double othMag = Math.sqrt(Math.pow(otherBall.velocity[0],2) + Math.pow(otherBall.velocity[1],2));
         
         double diffX = position[0]-otherBall.position[0];
         double diffY = position[1]-otherBall.position[1];
-
-        velocity = new double[] {velMag * diffX/dist, velMag * diffY/dist};
-        otherBall.velocity = new double[] {-othMag * diffX/dist, -othMag * diffY/dist};
-
-
+        
+        velocity = new double[] {elasticness * velMag * diffX/dist, elasticness * velMag * diffY/dist};
+        position = new double[] {position[0]+velocity[0], position[1]+velocity[1]};
+        
+        otherBall.velocity = new double[] {elasticness * -othMag * diffX/dist, elasticness * -othMag * diffY/dist};
+        otherBall.position = new double[] {otherBall.position[0]+otherBall.velocity[0], otherBall.position[1]+otherBall.velocity[1]};
+        return true;
     }
     
-    public void collide(Square otherSquare) {
-        // collides with a Square
-        return;
+    public boolean collide(Square other) {
+        
+        double[][] verts = {
+            {other.getPos()[0]+other.getLen(),other.getPos()[1]+other.getLen()},
+            {other.getPos()[0]+other.getLen(),other.getPos()[1]               },
+            {other.getPos()[0]+other.getLen(),other.getPos()[1]-other.getLen()},
+            {other.getPos()[0]               ,other.getPos()[1]-other.getLen()},
+            {other.getPos()[0]-other.getLen(),other.getPos()[1]-other.getLen()},
+            {other.getPos()[0]-other.getLen(),other.getPos()[1]               },
+            {other.getPos()[0]-other.getLen(),other.getPos()[1]+other.getLen()},
+            {other.getPos()[0]               ,other.getPos()[1]+other.getLen()}
+        };
+
+        boolean collided = false;
+        double dist = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < verts.length; i++) {
+            dist = Math.sqrt(Math.pow(verts[i][0] - position[0],2) + Math.pow(verts[i][1] - position[1],2));
+            if (dist < radius) {collided = true; break;}
+        }
+        if (!collided) return false;
+
+        dist = Math.sqrt(Math.pow(other.getPos()[0] - position[0],2) + Math.pow(other.getPos()[0] - position[1],2));
+        double velMag = Math.sqrt(Math.pow(velocity[0],2) + Math.pow(velocity[1],2));
+        double othMag = Math.sqrt(Math.pow(other.getVel()[0],2) + Math.pow(other.getVel()[1],2));
+        
+        double diffX = position[0]-other.getPos()[0];
+        double diffY = position[1]-other.getPos()[1];
+        
+        velocity = new double[] {elasticness * velMag * diffX/dist, elasticness * velMag * diffY/dist};
+        position = new double[] {position[0]+velocity[0], position[1]+velocity[1]};
+        
+        other.setVel(new double[] {elasticness * -othMag * diffX/dist, elasticness * -othMag * diffY/dist});
+        other.setPos(new double[] {other.getPos()[0]+(other.getVel()[0]), other.getPos()[1]+other.getVel()[1]});
+
+        return true;
     }
 
+    public void draw() {
+        StdDraw.setPenColor(color);
+        StdDraw.filledCircle(position[0], position[1], radius);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.circle(position[0], position[1], radius);
+    }
     public double[] getPos() {
         return position;
     }
--- Sandbox.java
+++ Sandbox.java
@@ -1,22 +1,52 @@
+import java.util.ArrayList;
+import java.awt.Color;
+
 public class Sandbox {
     public static void main(String[] args) {
+        final Color[] colors = {StdDraw.RED, StdDraw.ORANGE, StdDraw.YELLOW, StdDraw.GREEN, StdDraw.BLUE, StdDraw.MAGENTA, StdDraw.BLACK, StdDraw.CYAN, StdDraw.LIGHT_GRAY, StdDraw.PINK, StdDraw.PINK, StdDraw.YELLOW, StdDraw.DARK_GRAY};
+
         System.out.println("exampletext");
-        Ball ball1 = new Ball();
-        Ball ball2 = new Ball(new double[] {0,0.5}, new double[] {0,0}, 0.1, 0.9);
+        Ball[] balls = new Ball[20];
+        Square[] squares = new Square[20];
+        for (int i = 0; i < balls.length; i++) {
+            balls[i] = new Ball(new double[] { 1.8 * Math.random() - 0.9,1.8 * Math.random() - 0.9}, new double[] {(Math.random()-0.5)/10,(Math.random()-0.5)/10}, 0.08, 0.9, colors[(int)(Math.random() * colors.length)]);
+        }
+        for (int i = 0; i < squares.length; i++) {
+            squares[i] = new Square(new double[] { 1.8 * Math.random() - 0.9,1.8 * Math.random() - 0.9}, new double[] {(Math.random()-0.5)/10,(Math.random()-0.5)/10}, 0.08, 0.9, colors[(int)(Math.random() * colors.length)]);
+        }
+
         StdDraw.setXscale(-1.0, 1.0);
         StdDraw.setYscale(-1.0, 1.0);
         StdDraw.enableDoubleBuffering();
         
-
         while (true) {
             StdDraw.clear(StdDraw.WHITE);
-            ball1.move();
-            ball2.move();
-            ball1.bounceWall();
-            ball2.bounceWall();
-            ball1.collide(ball2);
-            StdDraw.filledCircle(ball1.getPos()[0], ball1.getPos()[1], ball1.getRad());
-            StdDraw.filledCircle(ball2.getPos()[0], ball2.getPos()[1], ball2.getRad());
+            
+            ArrayList<int[]> ballCollisions = new ArrayList<int[]>();
+            ArrayList<int[]> squareCollisions = new ArrayList<int[]>();
+            ArrayList<int[]> bsCollisions = new ArrayList<int[]>();
+            
+            for (int i = 0; i < balls.length; i++) {
+                balls[i].bounceWall();
+                
+                for (int j = i+1; j < balls.length; j++) if (!ballCollisions.contains(new int[] {i,j}) && !ballCollisions.contains(new int[] {j,i})) {
+                    if (balls[i].collide(balls[j])) ballCollisions.add(new int[] {i,j});
+                }
+                for(int j = 0; j < squares.length; j++) if (!bsCollisions.contains(new int[] {i,j}))
+                if (balls[i].collide(squares[j])) bsCollisions.add(new int[] {i,j});
+                balls[i].move(); 
+                balls[i].draw();
+            }
+            for (int i = 0; i < squares.length-1; i++) {
+                squares[i].bounceWall();
+                
+                for (int j = i+1; j < squares.length; j++) if (!squareCollisions.contains(new int[] {i,j}) && !squareCollisions.contains(new int[] {j,i})) {
+                    if (squares[i].collide(squares[j])) squareCollisions.add(new int[] {i,j});
+                }
+                
+                squares[i].move(); 
+                squares[i].draw();
+            }
             StdDraw.show();
             StdDraw.pause(20);
         }
